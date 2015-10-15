05563a57fbd58766fa878d623e3c1826cd39ddb6
Attila Körmendi <starbld@gmail.com>
2015-10-15 10:42:32 +0200

    2st nya tester i TestCustomer

--- src/test/inte/projekt/TestCustomer.java
+++ src/test/inte/projekt/TestCustomer.java
@@ -13,4 +13,15 @@ public class TestCustomer {
         Customer c = new Customer("192001013311","Namn Efternamn", "Gata", "nr", true);
         assertEquals("192001013311",c.getPersonnr());
     }
+
+    @Test (expected = Exception.class)
+    public void testPersonnrMonthValidity(){
+        Customer c = new Customer("192013013311","Namn Efternamn", "Gata", "nr", true);
+    }
+
+    @Test (expected = Exception.class)
+    public void testPersonnrDateValidity(){
+        Customer c = new Customer("192001353311","Namn Efternamn", "Gata", "nr", true);
+    }
+    
 }
