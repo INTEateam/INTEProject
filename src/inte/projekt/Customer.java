03213f28e01650e19a5c4829d850a449058db42a
Attila Körmendi <starbld@gmail.com>
2015-10-16 15:49:40 +0200

    Lade till fler tester till Customer för validering av datum och byggde de metoder som validerar dessa

--- src/inte/projekt/Customer.java
+++ src/inte/projekt/Customer.java
@@ -1,5 +1,11 @@
 package inte.projekt;
 
+import freemarker.template.SimpleDate;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Nicklas on 2015-10-14.
  */
@@ -28,10 +34,14 @@ public class Customer {
         return personnr;
     }
 
-    private void setPersonnr(String personnr) throws IllegalArgumentException{
+    private void setPersonnr(String personnr){
         if(personnr.length() == 10){
-            //String datepart = personnr.substring(0,6);
-            this.personnr = personnr;
+            String datepart = personnr.substring(0,6);
+            if(validDate(datepart,10)){
+                this.personnr = personnr;
+            }else{
+                throw new IllegalArgumentException("Invalid personnr.");
+            }
         }else if(personnr.length() == 12){
             if(personnr.charAt(0) == '1' && personnr.charAt(1) == '9'){
                 this.personnr = personnr;
@@ -63,5 +73,26 @@ public class Customer {
 
     public void setMember(Boolean member) {
         this.member = member;
+    }
+
+    public boolean validDate(String dateToCheck,int length){
+        if(length == 10){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+            sdf.setLenient(false);
+            try{
+                Date date = sdf.parse(dateToCheck);
+            }catch (ParseException e){
+                return false;
+            }
+        }else if(length == 12){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+            sdf.setLenient(false);
+            try{
+                Date date = sdf.parse(dateToCheck);
+            }catch (ParseException e){
+                return false;
+            }
+        }
+        return true;
     }
 }
