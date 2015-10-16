de71f95fd98d390a24a55b0fe1e4549234e604cf
Andre <andre.siggesjo@hotmail.se>
2015-10-16 15:51:42 +0200

    discount å shiz

---
+++ src/inte/projekt/Discount.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inte.projekt;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+/**
+ *
+ * @author andre
+ */
+public class Discount {
+
+    Product p;
+    BigDecimal discountAmount;
+
+    public Discount(BigDecimal discountAmount, Product p) {
+        if (discountAmount.compareTo(BigDecimal.ZERO) > 0 && discountAmount.compareTo(new BigDecimal(0.7)) <= 0) {
+            this.discountAmount = new BigDecimal(1).subtract(discountAmount);
+            this.p = p;
+
+        } else {
+            throw new IllegalArgumentException("ken dum jävel");
+        }
+
+    }
+
+    public Product getP() {
+        return p;
+    }
+
+    public void setP(Product p) {
+        this.p = p;
+    }
+
+    public BigDecimal getPriceWithDiscount() {
+        p.price = p.price.multiply(this.discountAmount);
+        p.price = p.price.setScale(0, BigDecimal.ROUND_HALF_UP);
+
+        return p.price;
+    }
+
+    public BigDecimal getDiscountAmount() {
+
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(BigDecimal discountAmount) {
+
+        this.discountAmount = new BigDecimal(1).subtract(discountAmount);
+
+    }
+
+}
