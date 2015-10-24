3ff08478fd98e863edb2f7ce10a775db5eec6982
Nicklas Andersson <nicklas@wwn.se>
2015-10-24 17:19:36 +0200

    passes add test

--- src/inte/projekt/Money.java
+++ src/inte/projekt/Money.java
@@ -4,12 +4,18 @@ package inte.projekt;
  * Created by Nicklas on 2015-10-24.
  */
 public class Money {
-    Money(int amount){
+    int amount;
 
+    Money(int amount){
+        this.amount = amount;
+    }
+
+    public void add(int amountToAdd){
+        this.amount = this.amount + amountToAdd;
     }
 
     @Override
     public String toString() {
-        return "10.00";
+        return amount + ".00";
     }
 }
