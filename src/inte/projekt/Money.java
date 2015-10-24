7c33ea17e267976839603abd597f3893a52be784
Nicklas Andersson <nicklas@wwn.se>
2015-10-24 17:11:07 +0200

    Money first test, passes

--- src/inte/projekt/Money.java
+++ src/inte/projekt/Money.java
@@ -4,4 +4,12 @@ package inte.projekt;
  * Created by Nicklas on 2015-10-24.
  */
 public class Money {
+    Money(int amount){
+
+    }
+
+    @Override
+    public String toString() {
+        return "10.00";
+    }
 }
