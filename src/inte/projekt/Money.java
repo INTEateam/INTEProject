ebde5875ce753c90c5913cd5d2078495ff56139b
Nicklas Andersson <nicklas@wwn.se>
2015-10-25 11:22:30 +0100

    Change to BigDecimal to enable percent calculations.  DecreasePercent broken

--- src/inte/projekt/Money.java
+++ src/inte/projekt/Money.java
@@ -1,25 +1,32 @@
 package inte.projekt;
 
+import java.math.BigDecimal;
+
 /**
  * Created by Nicklas on 2015-10-24.
  */
 public class Money {
-    int amount;
+    private final static int TWO_DECIMALS = 2;
+    private BigDecimal amount;
 
     Money(int amount){
-        this.amount = amount;
+        this.amount = new BigDecimal(amount).setScale(TWO_DECIMALS, BigDecimal.ROUND_HALF_UP);
     }
 
     public void add(int amountToAdd){
-        this.amount = this.amount + amountToAdd;
+        this.amount = this.amount.add(new BigDecimal(amountToAdd));
     }
 
     public void subtract(int amountToSubtract){
-        this.amount = this.amount - amountToSubtract;
+        this.amount = this.amount.subtract(new BigDecimal(amountToSubtract));
+    }
+
+    public void decreasePercent(int percent){
+        this.amount = this.amount.multiply(new BigDecimal(percent).movePointLeft(2));
     }
 
     @Override
     public String toString() {
-        return amount + ".00";
+        return this.amount.toString();
     }
 }
