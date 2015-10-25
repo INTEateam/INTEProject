c2c6e569fb7d89aa3bde8fef7da20492749f2027
Nicklas Andersson <nicklas@wwn.se>
2015-10-25 11:31:34 +0100

    DecreasePercent fixed

--- src/inte/projekt/Money.java
+++ src/inte/projekt/Money.java
@@ -1,32 +1,37 @@
 package inte.projekt;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 /**
  * Created by Nicklas on 2015-10-24.
  */
 public class Money {
     private final static int TWO_DECIMALS = 2;
+    private final static MathContext context = new MathContext(TWO_DECIMALS, RoundingMode.HALF_UP);
     private BigDecimal amount;
 
-    Money(int amount){
-        this.amount = new BigDecimal(amount).setScale(TWO_DECIMALS, BigDecimal.ROUND_HALF_UP);
+    Money(int amount) {
+        this.amount = new BigDecimal(amount, context);
     }
 
-    public void add(int amountToAdd){
+    public void add(int amountToAdd) {
         this.amount = this.amount.add(new BigDecimal(amountToAdd));
     }
 
-    public void subtract(int amountToSubtract){
+    public void subtract(int amountToSubtract) {
         this.amount = this.amount.subtract(new BigDecimal(amountToSubtract));
     }
 
-    public void decreasePercent(int percent){
-        this.amount = this.amount.multiply(new BigDecimal(percent).movePointLeft(2));
+    public void decreasePercent(int percent) {
+        BigDecimal decimalPercent = new BigDecimal(10).movePointLeft(2);
+        BigDecimal amountToRemove = this.amount.multiply(decimalPercent, context);
+        this.amount = this.amount.subtract(amountToRemove, context);
     }
 
     @Override
     public String toString() {
-        return this.amount.toString();
+        return this.amount.setScale(TWO_DECIMALS).toString();
     }
 }
